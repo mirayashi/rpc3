7be3fe02f1a7cf2d11a5725f371f1d8a03737a2e
Mirayashi <mirayashi@proton.me>
2023-10-02 23:16:25 +0200

    Fix server registration error handling

--- apps/counter-server-app/src/index.ts
+++ apps/counter-server-app/src/index.ts
@@ -5,13 +5,11 @@ import { getRequestListener } from './getRequestListener.js'
 const server = await RPC3Server.create(config)
 const requestListener = await getRequestListener(server)
 
-for (;;) {
-  try {
-    await server.ensureIsRegistered()
-    break
-  } catch (err) {
-    console.error('Unable to register, stake requirement changed. Retrying...')
-  }
+try {
+  await server.ensureIsRegistered()
+} catch (err) {
+  console.error('Registration failed', typeof err === 'object' && err != null && 'message' in err ? err.message : err)
+  process.exit(1)
 }
 server.listenToRequests(requestListener)
 
